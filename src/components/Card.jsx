eca6aed179632ebdbe7f16193516e5e68c0dac91
janeth <janeth.cr.mdz@gmail.com>
2024-02-29 17:32:55 -0500

    establecer contenido de cada tarjeta

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import "./Card.css";//importamos el css para poder aplicar estilos
+
+function Card() {
+  return (
+    <div className='Card'>
+        <h2>Titulo tarjeta</h2>
+        <p>descripcion tarjeta</p>
+    </div>
+  )
+}
+
+export default Card
